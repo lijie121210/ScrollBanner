1ffd877bc46d4b3e0f219783452ab65926e3d40a
viwii <lijie121210@163.com>
2016-12-26 10:36:35 +0800

    Initial Commit

---
+++ ScrollBanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollBanner
+//
+//  Created by jie on 2016/12/26.
+//  Copyright © 2016年 HTIOT.Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
